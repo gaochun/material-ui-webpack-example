2ecff1df717d6ae82f900258b65bd46893ecead0
Gao Chun <chun.gao@intel.com>
2016-12-26 00:52:33 +0800

    Add my page.

--- src/app/Main.js
+++ src/app/Main.js
@@ -4,18 +4,21 @@
  */
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import Subheader from 'material-ui/Subheader';
+import {List, ListItem} from 'material-ui/List';
 import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const styles = {
-  container: {
-    textAlign: 'center',
-    paddingTop: 200,
-  },
-};
+import Divider from 'material-ui/Divider';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import FileAttachment from 'material-ui/svg-icons/file/attachment';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -28,50 +31,115 @@ class Main extends Component {
     super(props, context);
 
     this.state = {
-      open: false,
+      open: document.body.clientWidth > 990,
+      docked: document.body.clientWidth > 990
     };
   }
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
+  handleLeftIconButtonTouchTap = (event) => {
+    this.setState({open: !this.state.open});
+    console.log("clicked");
   }
 
-  handleTouchTap = () => {
-    this.setState({
-      open: true,
-    });
+  updateDimensions = function() {
+    this.setState((prevState, props) => ({
+      open: document.body.clientWidth > 990 ? true : prevState.open,
+      docked: document.body.clientWidth > 990
+    }));
+  }
+  componentWillMount = function() {
+    this.updateDimensions();
+  }
+  componentDidMount = function() {
+    window.addEventListener("resize", () => this.updateDimensions());
+  }
+  componentWillUnmount = function() {
+    window.removeEventListener("resize", () => this.updateDimensions());
   }
 
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
-
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
+        <AppBar
+          title="设计器"
+          onLeftIconButtonTouchTap={this.handleLeftIconButtonTouchTap}
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        >
+          <Drawer
+            docked={this.state.docked}
+            width={260}
             open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
+            onRequestChange={(open) => this.setState({open})}
           >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
+            <AppBar title="AppBar" showMenuIconButton={false} />
+            <List>
+              <Subheader>Nested List Items</Subheader>
+              <ListItem primaryText="表单"
+                leftIcon={<ContentInbox />}
+                initiallyOpen={true}
+                primaryTogglesNestedList={true}
+                nestedItems={[
+                  <ListItem
+                    key={1}
+                    primaryText="单据"
+                    leftIcon={<FileFolder />}
+                    initiallyOpen={true}
+                    primaryTogglesNestedList={true}
+                    nestedItems={[
+                      <ListItem key={1} primaryText="客户订单" leftIcon={<FileAttachment />}/>,
+                      <ListItem key={2} primaryText="出库单" leftIcon={<FileAttachment  />}/>
+                    ]}
+                  />,
+                  <ListItem
+                    key={2}
+                    primaryText="字典"
+                    leftIcon={<FileFolder />}
+                  />,
+                  <ListItem
+                    key={3}
+                    primaryText="报表"
+                    leftIcon={<FileFolder />}
+                  />
+                ]}
+              />
+              <Divider />
+              <ListItem primaryText="业务蓝图" leftIcon={<ContentDrafts />} />
+              <Divider />
+              <ListItem
+                primaryText="Inbox"
+                leftIcon={<ContentInbox />}
+                initiallyOpen={true}
+                primaryTogglesNestedList={true}
+                nestedItems={[
+                  <ListItem
+                    key={1}
+                    primaryText="Starred"
+                    leftIcon={<ActionGrade />}
+                  />,
+                  <ListItem
+                    key={2}
+                    primaryText="Sent Mail"
+                    leftIcon={<ContentSend />}
+                    disabled={true}
+                    nestedItems={[
+                      <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
+                    ]}
+                  />,
+                  <ListItem
+                    key={3}
+                    primaryText="Inbox"
+                    leftIcon={<ContentInbox />}
+                    open={this.state.open}
+                    onNestedListToggle={this.handleNestedListToggle}
+                    nestedItems={[
+                      <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
+                    ]}
+                  />,
+                ]}
+              />
+            </List>
+          </Drawer>
+        </AppBar>
       </MuiThemeProvider>
     );
   }
